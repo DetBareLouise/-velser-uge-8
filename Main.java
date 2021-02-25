2865ab1d3cb42a35e0592d5f84f671a2717876b1
DetBareLouise <78900475+DetBareLouise@users.noreply.github.com>
2021-02-25 13:09:51 +0100

    TaskOne

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main{
+
+public static void main(String [] args){
+	
+	String name = "Louise";
+	int age = 23;
+
+
+	System.out.println("");
+
+	System.out.println("My name is" + name + "I am" + age + "years old");
+
+}
+}
